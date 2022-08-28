d891f69884665ad11c4573f6f639ca291b54fa89
pronepoet <ochiengphillip84@gmail.com>
2022-08-28 19:02:52 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let newWord = word.split('').reverse().join('')
+  // console.log(newWord)
+  if (newWord === word){
+    return true
+  } else{
+    return false
+  }
+
 }
 
 /* 
   Add your pseudocode here
+
+ -take a  name
+ -convert to an array
+  -reverse
+  -assign the reversed array to a new variable
+  -convert back to a string
+  -compare to the original namne
+  -return true or false
 */
 
 /*
   Add written explanation of your solution here
+
+  -first used the split method to split the srring into an array
+  -used the reverse method to rearrange the srray to the reversed arrangement
+  -used the join method to convert the reversed array to string
+  -compared the new string to the original string to see whether it still remains the same
+  -returned the value
+
 */
 
 // You can run `node index.js` to view these console logs
